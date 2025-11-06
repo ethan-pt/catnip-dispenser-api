c2b207ad4f0e11ad5e15a2afc28df65c4b345c2b
assentt <tubbeethan@gmail.com>
2025-11-05 21:54:16 -0800

    implement dispense request endpoint with API key validation

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Header
+from pydantic import BaseModel
 
 
 app = FastAPI(title="Catnip Dispenser API")
 
-@app.get("/")
-def read_root():
-    return {"message": "bug stinks"}
+API_KEY = "temp_key" # this will be moved to a .env file, just testing stuff for now
+
+class DispenseRequest(BaseModel):
+    device_id: str
 
 @app.post("/api/v1/dispense/request")
-def dispense():
-    return {"allowed": True}
+def dispense(request: DispenseRequest, x_api_key: str = Header()):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key.")
+
+    print(f"Received request from device: {request.device_id}")
+    
+    return {
+        "allowed": True,
+        "device_id": request.device_id
+    }
